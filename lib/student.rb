81923be694c04fefc54935c60f8f2701964147de
rupinderkaur30-gif <rupi063094@gmail.com>
2021-04-10 19:20:50 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,51 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
-
+require 'pry'
 class Student < InteractiveRecord
+ attr_accessor :id ,:name, :grade
+ def self.table_name
+    self.to_s.downcase.pluralize
+ end
 
+ def self.column_names
+     DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+    table_info =  DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+        column_names << column["name"]
+    end
+    column_names.compact
+end
+
+def initialize(options={})
+    options.each do |key, value|
+    self.send("#{key}=", value)
+    end
+end
+
+def table_name_for_insert
+    self.class.table_name
+end
+
+def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+end
+
+def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+        values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+end
+
+def save
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
+    binding.pry
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+end
 end
